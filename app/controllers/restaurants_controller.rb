19a1cf4512f9678160d5e6a604f1c5887f69a5ae
Gilad Sarusi <giladsar@gmail.com>
2019-09-03 16:02:16 +0300

    done

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,31 @@
+require 'byebug'
+class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(set_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+    @reviews = @restaurant.reviews
+  end
+
+ private
+
+  def set_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+one = {
+  name: "Cafe",
+  address: "21 jump street",
+  category: "chinese",
+  phone_number: "054-836-8369"
+}
+Restaurant.create(one)
+two = {
+  name: "Cafe earupe",
+  address: "21 Hameyasdim",
+  category: "chinese",
+  phone_number: "054-826-8369"
+}
+Restaurant.create(two)
+three = {
+  name: "SUSU",
+  address: "21 jump haven",
+  category: "italian",
+  phone_number: "052-836-8369"
+}
+Restaurant.create(three)
+four = {
+  name: "lololo",
+  address: "21 hell street",
+  category: "italian",
+  phone_number: "094-836-8369"
+}
+Restaurant.create(four)
+five = {
+  name: "Cafe Cafe",
+  address: "Dicken street",
+  category: "french",
+  phone_number: "055-836-8369"
+}
+Restaurant.create(five)
